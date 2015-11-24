dd8189dd4ed0014298f9cc4fd37b601601d598fb
hzhong@parallel <he.chron@gmail.com>
2015-11-23 17:09:37 -0700

    set up gfortran & ifort compiler flags

---
+++ cmake/options.cmake
@@ -0,0 +1,15 @@
+if(ST_WITH_INTEL)
+	find_program(CMAKE_C_COMPILER NAMES icc)
+	find_program(CMAKE_CXX_COMPILER NAMES icpc)
+	find_program(CMAKE_Fortran_COMPILER NAMES ifort)
+	find_program(CMAKE_AR NAMES xiar)
+	find_program(CMAKE_LINKER NAMES xild)
+	if(CMAKE_C_COMPILER MATCHES CMAKE_C_COMPILER-NOTFOUND OR
+			CMAKE_CXX_COMPILER MATCHES CMAKE_CXX_COMPILER-NOTFOUND OR
+			CMAKE_Fortran_COMPILER MATCHES CMAKE_Fortran_COMPILER-NOTFOUND OR
+			CMAKE_AR MATCHES CMAKE_AR-NOTFOUND OR
+			CMAKE_LINKER MATCHES CMAKE_LINKER-NOTFOUND)
+		message(WARNING "Cannot find Intel compilers. Leave CMake to find the default compilers.")
+	endif()
+endif()
+
